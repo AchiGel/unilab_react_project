04bf2f51a947971ab75871c9b84f95b7f52bf83d
AchiGel <ar.gelashvili@gmail.com>
2024-04-06 16:43:35 +0400

    add data and services cards

---
+++ src/components/ServiceCard.js
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components";
+
+const ServiceCardContainer = styled.div`
+  max-width: 380px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const ServiceCardImg = styled.img`
+  width: 100%;
+  object-fit: cover;
+  border-radius: 20px;
+`;
+
+const ServiceCardHeader = styled.h2`
+  color: #424244;
+  font-size: 32px;
+  font-weight: 500;
+`;
+
+const ServiceCardText = styled.p`
+  color: rgba(37, 37, 37, 0.8);
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+function ServiceCard({ img, header, text }) {
+  return (
+    <ServiceCardContainer>
+      <ServiceCardImg src={img} alt={img} />
+      <ServiceCardHeader>{header}</ServiceCardHeader>
+      <ServiceCardText>{text}</ServiceCardText>
+    </ServiceCardContainer>
+  );
+}
+
+export default ServiceCard;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,49 @@
+import styled, { css } from "styled-components";
+import ServiceCard from "./ServiceCard";
+import servicesData from "../DATA/servicesCards.json";
+
+const ServicesHeader = styled.h2`
+  color: #424244;
+  font-size: 40px;
+  font-weight: 500;
+  margin-bottom: 20px;
+`;
+
+const ServicesDescrib = styled.p`
+  color: rgba(37, 37, 37, 0.8);
+  font-size: 20px;
+  font-weight: 400;
+`;
+
+const ServicesCards = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
 function Services() {
-  return <section>Services</section>;
+  const partOfData = servicesData.slice(0, 4);
+  return (
+    <section style={{ marginTop: "60px" }}>
+      <ServicesHeader>Discover Our Services</ServicesHeader>
+      <ServicesDescrib>
+        ExploreEra provides a one-stop solution for individuals seeking
+        well-planned journeys. These services include expert advice on
+        destination selection, flight and accommodation bookings, and customized
+        itineraries to individual preferences.
+      </ServicesDescrib>
+      <ServicesCards>
+        {partOfData.map((item) => (
+          <ServiceCard
+            key={item.id}
+            img={item.img}
+            header={item.header}
+            text={item.descr}
+          />
+        ))}
+      </ServicesCards>
+    </section>
+  );
 }
 
 export default Services;
