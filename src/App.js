2419bf1ea9dc927078b012cde857dcc9be05b854
AchiGel <ar.gelashvili@gmail.com>
2024-04-08 13:40:20 +0400

    set router settings

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import AuthentificationPage from "./components/AuthentificationPage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/signin",
+    element: <AuthentificationPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
-        <Main />
+        <RouterProvider router={router} />
         <Footer />
       </div>
     </div>
